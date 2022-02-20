f0b2413469d26e17114b71882e6b6079262e49b2
Genaro Javier <genaro120300@gmail.com>
2022-02-20 10:40:55 -0600

    Expresiones y propiedades

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app', 
+
+    data () {
+        return {
+            titulo: 'Hola mundo',
+            imagen: 'https://images.unsplash.com/photo-1645307115996-f0cb8111c783?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=424&q=80',
+        }
+    }
+})
