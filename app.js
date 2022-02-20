58dce948d924b4be29880211357196b7476fd986
Genaro Javier <genaro120300@gmail.com>
2022-02-20 12:31:26 -0600

    Mostrar y ocultar elementos

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ new Vue({
 
     data () {
         return {
-            titulo: 'Hola mundo',
-            imagen: 'https://images.unsplash.com/photo-1645307115996-f0cb8111c783?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=424&q=80',
+            check_1: false,
+            img_1: "https://images.unsplash.com/photo-1645352809168-be1d1ef6d31c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80",
+
+            check_2: false,
+            img_2: "https://images.unsplash.com/photo-1645342749970-a643ad2ad28b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+
+            check_3: false,
+            img_3: "https://images.unsplash.com/photo-1645359271414-3697b7a5781f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=914&q=80",
         }
     }
 })
