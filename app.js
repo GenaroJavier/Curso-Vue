200d75fb59cba84f72cee0ae2e0cb56bab7104b6
Genaro Javier <genaro120300@gmail.com>
2022-02-20 18:28:55 -0600

    For

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ new Vue({
 
     data () {
         return {
-            check_1: false,
-            img_1: "https://images.unsplash.com/photo-1645352809168-be1d1ef6d31c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80",
-
-            check_2: false,
-            img_2: "https://images.unsplash.com/photo-1645342749970-a643ad2ad28b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-
-            check_3: false,
-            img_3: "https://images.unsplash.com/photo-1645359271414-3697b7a5781f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=914&q=80",
+            animales: ['burro', 'caballo', 'perro', 'gato', 'raton'],
+            personas: [
+                {nombre: "Genaro", apellido: "perez", edad: 21, puesto: "residente"},
+                {nombre: "Jesus", apellido: "perez", edad: 15, puesto: "alumno"},
+                {nombre: "Oliver", apellido: "camargo", edad: 21, puesto: "alumno"},
+                {nombre: "Mahetsi", apellido: "perez", edad: 21, puesto: "alumno"},
+                {nombre: "Efrain", apellido: "Rosas", edad: 46, puesto: "comerciante"},
+            ]
+           
         }
     }
 })
