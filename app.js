3cbd9133d96bb8145a76e233d160c097b435df41
Genaro Javier <genaro120300@gmail.com>
2022-03-03 12:21:37 -0600

    Manejo del v-on

--- app.js
+++ app.js
@@ -10,8 +10,21 @@ new Vue({
                 {nombre: "Oliver", apellido: "camargo", edad: 21, puesto: "alumno"},
                 {nombre: "Mahetsi", apellido: "perez", edad: 21, puesto: "alumno"},
                 {nombre: "Efrain", apellido: "Rosas", edad: 46, puesto: "comerciante"},
-            ]
-           
+            ],
+            mostrar_elementos: false,
+        }
+    },
+    methods: {
+        /**
+         * Es curioso como funciona este metodo, por que en un inicio especificamos 
+         * la variable encargada de mostrar y ocultar un elemento, 
+         * por ello la inicalizamos en false, para que este metodo me cambie por completo
+         * dicho valor, es decir cada vez que el evento click se dispare, se ejecuta
+         * el metodo y la variable cambie su valor lo que nos permite ocultar y mostrar
+         * el div contenedor.
+         */
+        Mostrar_Precios () {
+            this.mostrar_elementos = !this.mostrar_elementos; 
         }
     }
 })
